46c73bfd03cc958a6d8af510adee0b7cee3290ab
qingjie <qzhao02@syr.edu>
2016-06-13 10:01:51 -0400

    mapboxdemo

---
+++ MapBoxDemo/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  MapBoxDemo
+//
+//  Created by qingjiezhao on 6/13/16.
+//  Copyright © 2016 dongyaocun. All rights reserved.
+//
+
+import UIKit
+import Mapbox
+
+class ViewController: UIViewController {
+
+    @IBOutlet var mapView: MGLMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let point = MGLPointAnnotation()
+        point.coordinate = CLLocationCoordinate2D(latitude: 45.52258,longitude: -122.6732)
+        
+        point.title = "Warm Springs"
+        point.subtitle = "Idaho, USA"
+        mapView.addAnnotation(point)
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func mapView(mapView:MGLMapView,annotationCanShowCallout annotation:MGLAnnotation) -> Bool{
+        return true
+    }
+}
+
